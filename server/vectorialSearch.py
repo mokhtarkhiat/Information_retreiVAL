c5611ad294ccb30203de79cb2c40cb40ea881617
mokhtarkhiat <mokhtarkhiat.47@yahoo.com>
2021-03-15 10:05:51 +0100

    evaluationb

--- server/vectorialSearch.py
+++ server/vectorialSearch.py
@@ -1,6 +1,7 @@
 # back end 
 import re 
 import nltk
+from nltk import text
 import numpy as np
 import string
 import math
@@ -106,22 +107,109 @@ def vectorialModelSearh(Query , matrixDoxumentTerm , similarityMeasure ,listWord
                 documentList[documentNumber+1]= somme
     
     
-    documentListResult = list(documentList.items())
-    documentListResult = sorted(documentListResult)
+    documentListResult = []
+    for key,value in documentList.items():
+        document = []
+        document.append(key)
+        document.append(value)
+        documentListResult.append(document)
+    documentListResult.sort(key = lambda x: x[1] , reverse=True)
     return documentListResult
 
 
 
+
+
 #evaluation module victorial 
+#read query
+def readQuery():
+    with open ("data/query.text") as query:
+        listLines = query.readlines() 
+        query = []
+        queryList = []
+        text=""
+
+        linesParcour = 0
+        while(linesParcour != len(listLines)):
+            line = listLines[linesParcour]
+            if (line.startswith(".I")):
+                queryNumber = int(line.split()[1])
+                if query != []:
+                    query.append(text)
+                    queryList.append(query)
+                
+                query = []
+                query.append(queryNumber)
+
+            if (line.startswith(".W")):
+                linesParcour = linesParcour + 1
+                while((nltk.re.findall('\.([TWBANX]\n|I d+\n)',listLines[linesParcour]) == []) and (linesParcour < len (listLines) )):
+                    text = text + "".join(listLines[linesParcour].split("\n")[0].lower())
+                    linesParcour = linesParcour + 1
+                linesParcour = linesParcour - 1    
+            linesParcour = linesParcour + 1
+
+        query.append(text)
+        queryList.append(query)        
+
+       
+    return queryList
+
+
+#read qrels
+
+def readQrels():
+    with open('data/qrels.text') as qrels :
+        listLines = qrels.readlines()
+
+        qrelList = nltk.defaultdict(list)
+
+        for line in listLines:
+            splitedLine = line.split()
+            qrelList[int(splitedLine[0])].append(int(splitedLine[1]))
+
+          
+
+    return qrelList
+
 #calculer recall
 
-def calculeRecall():
+def calculeRecall(queryNumber , resultList , qrelList ):
+    documentListResult = []
 
-    return
+    for result in resultList:
+   
+        documentListResult.append(result[0])
+
+    resultSet= set(documentListResult)
+    qrelSet  = set(qrelList) 
+    print(qrelList)
+    intersectionList = list(resultSet.intersection(qrelSet))
+
+    recall = 0.0
+
+    if len(qrelList) != 0:
+        recall = (float(len(intersectionList)) / float(len(qrelList)))
+
+    return recall
 #calculate Precision
-def calculatePrecision():
+def calculatePrecision(queryNumber , resultList , qrelList ):
+    documentsListResult = []
+
+    for result in resultList:
+        documentsListResult.append(result[0])
+
+    resultSet= set(documentsListResult)
+    qrelSet  = set(qrelList) 
+
+    intersectionList = list(resultSet.intersection(qrelSet))
+    precision = 0.0
     
-    return
+    if len(documentsListResult) != 0:
+        precision = (float(len(intersectionList)) / float(len(documentsListResult)))
+    
+   
+    return precision
     
 ###################################
 ###################################
@@ -142,23 +230,41 @@ if __name__ == '__main__':
     # saveInvertedFileWeights("data/invertedFileWeights.pkl",invertedFile_weights)
     matrixDoxumentTerm = preparationVectorialSearch(repetitionDict,invertedFile,len(documentList) )
     
-    query = 'Dictionary construction and accessing methods for fast retrieval of words or lexical items or morphologically related information. Hashing or indexing methods are usually applied to English spelling or natural language problems.'
     
-    similarityMeasure =  "Sørensen–Dice coefficient"#type de similariy 4
+    queryList=readQuery()
     listWords = list(repetitionDict.keys())
+    qrelList = readQrels()
+   
+    for query in queryList:
     
-    documentListResult = vectorialModelSearh(query , matrixDoxumentTerm  , similarityMeasure , listWords )
-    print(documentListResult)
-    
-    
-    similarityMeasure =  "Cosine similarity"#type de similariy 4
-    documentListResult = vectorialModelSearh(query , matrixDoxumentTerm  , similarityMeasure , listWords )
-    print(documentListResult)
-    
-    similarityMeasure =  "Jaccard index"#type de similariy 4
-    documentListResult = vectorialModelSearh(query , matrixDoxumentTerm  , similarityMeasure , listWords )
-    print(documentListResult)
+        print("Sørensen–Dice coefficient") 
+        
+        q=query[1]
+        similarityMeasure =  "Sørensen–Dice coefficient"#type de similariy 4
+        documentListResult = vectorialModelSearh(q , matrixDoxumentTerm  , similarityMeasure , listWords )
+        # print(documentListResult)  
+        print("recall of query    :",query[0],calculeRecall(query[0],documentListResult,qrelList[query[0]]))
+        print("precision of query    :",query[0],calculatePrecision(query[0],documentListResult,qrelList[query[0]]))
+        
 
-    similarityMeasure =  "Inner product"#type de similariy 4
-    documentListResult = vectorialModelSearh(query , matrixDoxumentTerm  , similarityMeasure , listWords )
-    print(documentListResult)
+        print("Cosine similarity")
+        similarityMeasure =  "Cosine similarity"#type de similariy 4
+        documentListResult = vectorialModelSearh(q , matrixDoxumentTerm  , similarityMeasure , listWords )
+        # print(documentListResult)
+        print("recall of query    :",query[0],calculeRecall(query[0],documentListResult,qrelList[query[0]]))
+        print("precision of query    :",query[0],calculatePrecision(query[0],documentListResult,qrelList[query[0]]))
+
+        print("Jaccard index")
+        similarityMeasure =  "Jaccard index"#type de similariy 4
+        documentListResult = vectorialModelSearh(q , matrixDoxumentTerm  , similarityMeasure , listWords )
+        # print(documentListResult)
+        print("recall of query    :",query[0],calculeRecall(query[0],documentListResult,qrelList[query[0]]))
+        print("precision of query    :",query[0],calculatePrecision(query[0],documentListResult,qrelList[query[0]]))
+
+        print("Inner product")
+        similarityMeasure =  "Inner product"#type de similariy 4
+        documentListResult = vectorialModelSearh(q , matrixDoxumentTerm  , similarityMeasure , listWords )
+        # print(documentListResult)
+        print("recall of query    :",query[0],calculeRecall(query[0],documentListResult,qrelList[query[0]]))
+        print("precision of query    :",query[0],calculatePrecision(query[0],documentListResult,qrelList[query[0]]))
+
